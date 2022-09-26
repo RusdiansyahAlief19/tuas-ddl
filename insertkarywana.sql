c1ecb8e6b5ebf8abe84b986856ae2fbc236a95a8
RusdiansyahAlief19 <110006590+RusdiansyahAlief19@users.noreply.github.com>
2022-09-26 14:09:46 +0700

    Add files via upload

---
+++ insertkarywana.sql
@@ -0,0 +1,13 @@
+insert into karyawan (nama,id_dep)
+values
+("agus","10"),
+("citra","16"),
+("indah","12"),
+("budi","17");
+
+insert into departemen(id_dep,nama_dep)
+values
+("10","Penelitian"),
+("11","Pemasaran"),
+("12","SDM"),
+("13","Keungan");
